c565d9352ad9b012d3f0b7d139b11d2715a07d42
dags- <dags@dags.me>
2018-07-13 12:03:40 +0100

    more

--- deploy/build.go
+++ deploy/build.go
@@ -1,14 +1,16 @@
 package deploy
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
-type Config struct {
-	Project string
-	Assets  []string
+type Project struct {
+	Owner  string
+	Name   string
+	Assets []string
 }
 
 func init() {
@@ -16,21 +18,38 @@ func init() {
 	exec.Command("go", "install", "github.com/dags-/bundler")
 }
 
-func Build(config *Config) (artifacts []string, e error) {
-	c := exec.Command("bundler", config.Project)
+func Build(project *Project) (artifacts []string, e error) {
+	path := fmt.Sprintf("github.com/%s/%s", project.Owner, project.Name)
+
+	c := exec.Command("bundler", path)
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	e = c.Run()
 	if e != nil {
 		return artifacts, e
 	}
-	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		for _, rule := range config.Assets {
+
+	filepath.Walk(workDir(path), func(path string, info os.FileInfo, err error) error {
+		for _, rule := range project.Assets {
 			if match, e := filepath.Match(rule, path); e == nil && match {
 				artifacts = append(artifacts, path)
 			}
 		}
 		return nil
 	})
+
 	return artifacts, nil
+}
+
+func workDir(path string) string {
+	if _, e := os.Stat(path); e == nil {
+		return path
+	}
+
+	path = filepath.Join(os.Getenv("GOPATH"), "src", path)
+	if _, e := os.Stat(path); e == nil {
+		return path
+	}
+
+	return "."
 }
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -17,8 +20,15 @@ var (
 	token  string
 	secret string
 	port   string
-	config *deploy.Config
+	queue  chan *build
+	config *deploy.Project
 )
+
+type build struct {
+	owner string
+	repo  string
+	id    int64
+}
 
 func init() {
 	t := flag.String("token", "", "github token")
@@ -30,34 +40,98 @@ func init() {
 	token = mustFlag("token", t)
 	port = mustFlag("port", p)
 
-	config = &deploy.Config{
-		Project: "github.com/dags-/launch",
+	config = &deploy.Project{
+		Owner: "dags-",
+		Name:  "launch",
 		Assets: []string{
-			"builds/darwin/*.zip",
-			"builds/windows/*.zip",
-			"builds/linux/*.AppImage",
+			"builds\\darwin\\*.zip",
+			"builds\\windows\\*.zip",
+			"builds\\linux\\*.AppImage",
 		},
 	}
 }
 
 func main() {
-	go handleStop()
+	go handleBuilds()
+	go handleCommands()
 	hook := github.New(&github.Config{Secret: secret})
-	hook.RegisterEvents(release, github.ReleaseEvent)
+	hook.RegisterEvents(handleRelease, github.ReleaseEvent)
 	e := webhooks.Run(hook, ":"+port, "/webhooks")
 	if e != nil {
 		panic(e)
 	}
 }
 
-func handleStop() {
+func handleCommands() {
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		line := strings.ToLower(s.Text())
 		if strings.HasPrefix(line, "stop") {
 			os.Exit(0)
 		}
+		if strings.HasPrefix(line, "build") {
+			b, e := latestRelease("dags-", "launch")
+			if e != nil {
+				log.Println("get release error:", e)
+			} else {
+				queue <- b
+			}
+		}
 	}
+}
+
+func handleRelease(payload interface{}, header webhooks.Header) {
+	r := payload.(github.ReleasePayload)
+	if r.Release.Draft || r.Release.Prerelease {
+		return
+	}
+
+	log.Println("release received:", r.Repository.Name)
+	queue <- &build{
+		owner: r.Repository.Owner.Login,
+		repo:  r.Repository.Name,
+		id:    r.Release.ID,
+	}
+}
+
+func handleBuilds() {
+	for {
+		b := <-queue
+		artifacts, e := deploy.Build(config)
+		if e != nil {
+			log.Println("deploy error:", e)
+			return
+		}
+
+		log.Printf("uploading %v artifacts\n", len(artifacts))
+		for _, artifact := range artifacts {
+			e := deploy.UploadAsset(b.owner, b.repo, b.id, artifact, token)
+			if e != nil {
+				log.Println("upload error:", e)
+			}
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func latestRelease(owner, repo string) (*build, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
+	r, e := http.Get(url)
+	if e != nil {
+		return nil, e
+	}
+	defer r.Body.Close()
+
+	var rel struct {
+		ID int64 `json:"id"`
+	}
+
+	e = json.NewDecoder(r.Body).Decode(&rel)
+	if e != nil {
+		return nil, e
+	}
+
+	return &build{owner: owner, repo: repo, id: rel.ID}, nil
 }
 
 func mustFlag(name string, val *string) string {
@@ -65,26 +139,4 @@ func mustFlag(name string, val *string) string {
 		panic("missing flag: " + name)
 	}
 	return *val
-}
-
-func release(payload interface{}, header webhooks.Header) {
-	r := payload.(github.ReleasePayload)
-	if r.Release.Draft || r.Release.Prerelease {
-		return
-	}
-
-	log.Println("release received:", r.Repository.Name)
-	artifacts, e := deploy.Build(config)
-	if e != nil {
-		log.Println("deploy error:", e)
-		return
-	}
-
-	for _, artifact := range artifacts {
-		e := deploy.UploadAsset("", r.Repository.Name, r.Release.ID, artifact, token)
-		if e != nil {
-			log.Println("upload error:", e)
-		}
-		time.Sleep(time.Second)
-	}
 }
